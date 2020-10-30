d03f773972516c1b27115d93ace9d0a085de03e8
Scott Ratigan <scotteratigan@gmail.com>
2020-10-30 15:45:49 -0700

    updates

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
 const pushState = () => {
   history.pushState('', '', ['somenewurl.html']);
 }
-
-// Show the function in the <pre> tag on the page:
-document.querySelector("#code").textContent = pushState.toString();
