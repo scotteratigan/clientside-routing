ad39e0a7d93187ec1885224c9e610c8a2264fe28
Scott Ratigan <scotteratigan@gmail.com>
2020-10-30 18:25:37 -0700

    added 5

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # clientside-routing
 
-Now that we have navigation working, let's navigate with links instead of a button.
+Now that we have custom links, let's look at rendering more like a framework might.
 
-To test this out, clone this repo, then `git checkout 4` and then serve the content locally. I recommend VS Code's Live Share Extension: https://marketplace.visualstudio.com/items?itemName=MS-vsliveshare.vsliveshare
+To test this out, clone this repo, then `git checkout 5` and then serve the content locally. I recommend VS Code's Live Share Extension: https://marketplace.visualstudio.com/items?itemName=MS-vsliveshare.vsliveshare
--- script.js
+++ script.js
@@ -1,34 +1,72 @@
-const contentDiv = document.querySelector('#content');
-
-const routes = {
-  "/index.html": `<p>This is the home page.</p>`,
-  "/about.html": `<p>This demonstrates dynamic content. <a href="index.html">Back to home</a></p>`,
-  "/mdn.html": `<a href="https://developer.mozilla.org/en-US/" target="_blank">MDN (normal link)</a>`,
-}
-
 function pushState(newUrl) {
   history.pushState('', '', [newUrl]);
-  loadContent();
-}
-
-function loadContent() {
-  const { pathname } = window.location;
-  const htmlContent = routes[pathname];
-  contentDiv.innerHTML = htmlContent;
+  render();
 }
 
 function handleClick(e) {
-  // capture all click events
   const { target } = e;
   const { customlink } = target.dataset;
-  // do nothing if custom link property is not present:
   if (!customlink) return;
-  // otherwise, prevent default navigation behavior:
   e.preventDefault();
   const newUrl = target.pathname;
   if (newUrl) pushState(newUrl)
 }
 
+// A specific function to render each component:
+function renderHome() {
+  return `<p>This is the home page.</p>` + renderNavbar();
+}
+
+function renderAbout() {
+  return `<p>This demonstrates dynamic content. <a href="index.html">Back to home</a></p>` + renderNavbar();
+}
+
+function renderMDN() {
+  return `<p>Check out the MDN docs: <a href="https://developer.mozilla.org/en-US/" target="_blank">MDN (normal link)</a></p>` + renderNavbar();
+}
+
+function render404() {
+  return `<p>Error, page not found.</p>` + renderNavbar();
+}
+
+function renderNavbar() {
+  const navbarLinks = [
+    { href: 'index.html', text: 'Home' },
+    { href: 'about.html', text: 'About' },
+    { href: 'mdn.html', text: 'MDN' },
+    { href: 'badurl1.html', text: 'Bad Url 1' },
+    { href: 'badurl2.html', text: 'Bad Url 2' },
+  ];
+  return navbarLinks.map(renderLink).join(' | ');
+}
+
+function renderLink(link) {
+  const { href, text } = link;
+  return `<a href="${href}" data-customlink="true">${text}</a>`;
+}
+
+// Generic render function uses the router to determine which content to load:
+function render() {
+  const { pathname } = window.location;
+  const htmlContent = router(pathname)
+  contentDiv.innerHTML = htmlContent;
+}
+
+// The router is really just a series of conditional checks:
+function router(route) {
+  switch (route) {
+    case '/index.html':
+      return renderHome();
+    case '/about.html':
+      return renderAbout();
+    case '/mdn.html':
+      return renderMDN();
+    default:
+      return render404();
+  }
+}
+
+const contentDiv = document.querySelector('#content');
 document.addEventListener('click', handleClick);
-window.onpopstate = loadContent;
-loadContent();
+window.onpopstate = render;
+render();
